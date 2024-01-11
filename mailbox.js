fd634182ed231c93e3906b84fb7f4df609f22cb7
towngoood <153721704+towngoood@users.noreply.github.com>
2024-01-11 15:31:57 +0800

    Create mailbox.js

---
+++ mailbox.js
@@ -0,0 +1,17 @@
+// 引用Office JavaScript API库
+<script src="https://appsforoffice.microsoft.com/lib/1/hosted/office.js" type="text/javascript"></script>
+
+// 调用Office 365 API
+Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function (result) {
+    var accessToken = result.value;
+    var getMessageUrl = Office.context.mailbox.restUrl + '/v2.0/me/messages';
+    $.ajax({
+        url: getMessageUrl,
+        dataType: 'json',
+        headers: { 'Authorization': 'Bearer ' + accessToken }
+    }).done(function (item) {
+        // 处理返回的数据
+    }).fail(function (error) {
+        // 处理错误
+    });
+});
